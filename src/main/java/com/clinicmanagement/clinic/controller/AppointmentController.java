9266994fd7f72bfed21d225ddf7008549e50a940
Toan-LD <101153912+Toan-LD@users.noreply.github.com>
2024-11-15 20:36:11 +0700

    thêm sửa xóa appointment success, getdoctorbyid thi` chua

--- src/main/java/com/clinicmanagement/clinic/controller/AppointmentController.java
+++ src/main/java/com/clinicmanagement/clinic/controller/AppointmentController.java
@@ -4,25 +4,45 @@ import com.clinicmanagement.clinic.Entities.Appointment;
 import com.clinicmanagement.clinic.dto.AppointmentDTO;
 import com.clinicmanagement.clinic.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api")
+@RequestMapping("/api/appointments")
 @RestController
 public class AppointmentController {
+    @Autowired
     private AppointmentService _appointmentService;
 
-    @Autowired
-    public AppointmentController(AppointmentService appointmentService) {
-        this._appointmentService = appointmentService;
+    @GetMapping
+    public List<Appointment> GetAllAppointments() {
+        return _appointmentService.GetAllUsers();
     }
 
-    @GetMapping("/appointment")
-    public List<Appointment> appointments(Model model) {
-        return _appointmentService.getAllUsers();
+    @GetMapping("/{id}")
+    public Appointment GetAppointmentById(@PathVariable int id) {
+        return _appointmentService.GetAppointmentById(id);
+    }
+
+    @GetMapping("/doctor/{id}")
+    public List<Appointment> GetAppointmentByDocTorId(@PathVariable int id) {
+        return _appointmentService.GetAppointmentByDocTorId(id);
+    }
+
+    @PostMapping
+    public Appointment CreateAppointment(@RequestBody AppointmentDTO appointmentDTO) {
+        return _appointmentService.AddAppointment(appointmentDTO);
+    }
+
+    @PutMapping
+    public Appointment UpdateAppointment(@RequestBody AppointmentDTO appointmentDTO) {
+        return _appointmentService.UpdateAppointment(appointmentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public String DeleteAppointment(@PathVariable int id) {
+        return _appointmentService.DeleteAppointment(id);
     }
 }
