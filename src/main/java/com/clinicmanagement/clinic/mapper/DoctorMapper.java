43620a217aa7547e5b2ef0f2f78642baf787882c
trunghieu <3121410197@sv.sgu.edu.vn>
2024-12-04 16:57:18 +0700

    04/12/2024

--- src/main/java/com/clinicmanagement/clinic/mapper/DoctorMapper.java
+++ src/main/java/com/clinicmanagement/clinic/mapper/DoctorMapper.java
@@ -1,12 +1,13 @@
 package com.clinicmanagement.clinic.mapper;
 
 import com.clinicmanagement.clinic.Entities.Doctor;
-import com.clinicmanagement.clinic.dto.DoctorDTO;
+import com.clinicmanagement.clinic.dto.doctor.DoctorCreateRequest;
+import com.clinicmanagement.clinic.dto.doctor.DoctorUpdateRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface DoctorMapper {
-    Doctor toDoctor(DoctorDTO doctorDTO);
-    void updateDoctorFromDto(DoctorDTO doctorDTO, @MappingTarget Doctor doctor);
+    Doctor toDoctor(DoctorCreateRequest request);
+    void updateDoctor(@MappingTarget Doctor doctor, DoctorUpdateRequest request);
 }
